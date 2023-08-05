628baa3ce54c22358ec8fd1f4aff5051a46a0672
CatalinStratu <catalinstratu45@gmail.com>
2023-08-05 17:38:00 +0300

    Code review changes

--- examples/InspectLibraries/main.go
+++ examples/InspectLibraries/main.go
@@ -1,8 +1,17 @@
+//
+// Copyright (c) nexB Inc. and others. All rights reserved.
+// ScanCode is a trademark of nexB Inc.
+// SPDX-License-Identifier: Apache-2.0
+// See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+// See https://github.com/nexB/scancode-toolkit for support or download.
+// See https://aboutcode.org for more information about nexB OSS projects.
+//
+
 package main
 
 import (
 	"fmt"
-	goInspector "github.com/nexB/go-inspector"
+	goinspector "github.com/nexB/go-inspector"
 	"os"
 )
 
@@ -14,7 +23,7 @@ func main() {
 		fmt.Printf("  print portions of its creation info data.\n")
 		return
 	}
-	jsonData, err := goInspector.ConvertToJSONWithPosixPaths(args[1])
+	jsonData, err := goinspector.ConvertToJSONWithPosixPaths(args[1])
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)

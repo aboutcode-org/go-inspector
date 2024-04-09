0979c7f32adbb726c7d27e34ceccffe0c8a0918b
Philippe Ombredanne <pombredanne@nexb.com>
2024-04-09 14:07:04 +0200

    Rename some test data files for clarity
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_go_inspector.py
+++ tests/test_go_inspector.py
@@ -4,7 +4,7 @@
 # ScanCode is a trademark of nexB Inc.
 # SPDX-License-Identifier: Apache-2.0
 # See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
-# See https://github.com/nexB/scancode-plugins for support or download.
+# See https://github.com/nexB/g-inspector for support or download.
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
@@ -13,34 +13,34 @@ import os
 
 import pytest
 
-from src.go_inspector.plugin import collect_and_parse_symbols
+from go_inspector.plugin import collect_and_parse_symbols
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 
-def test_goresym_with_windows_exe():
-    go_binary = os.path.join(TEST_DATA_DIR, "windows.exe")
+def test_collect_and_parse_symbols_with_plain_windows_exe():
+    go_binary = os.path.join(TEST_DATA_DIR, "plain_windows.exe")
     with pytest.raises(Exception) as e:
         collect_and_parse_symbols(go_binary)
 
 
-def test_goresym_with_elf():
-    go_binary = os.path.join(TEST_DATA_DIR, "arm_gentoo_elf")
+def test_collect_and_parse_symbols_with_plain_elf():
+    go_binary = os.path.join(TEST_DATA_DIR, "plain_arm_gentoo_elf")
     with pytest.raises(Exception) as e:
         collect_and_parse_symbols(go_binary)
 
 
 @pytest.mark.parametrize(
-    "path",
+    "exe_path",
     [
-        "app_lin_exe",
-        "app_mac_exe",
-        "app_win_exe",
+        "basic/app_lin_exe",
+        "basic/app_mac_exe",
+        "basic/app_win_exe",
     ],
 )
-def test_goresym_with_mini_go_app_linux(path):
-    go_binary = os.path.join(TEST_DATA_DIR, path)
+def test_collect_and_parse_symbols_with_mini_go_app_linux(exe_path):
+    go_binary = os.path.join(TEST_DATA_DIR, exe_path)
     goresym_output = collect_and_parse_symbols(go_binary)
-    with open(os.path.join(TEST_DATA_DIR, f"gore_sym_{path}_output.json")) as f:
+    with open(os.path.join(TEST_DATA_DIR, f"{exe_path}-goresym.json")) as f:
         expected_output = json.load(f)
         assert expected_output == goresym_output

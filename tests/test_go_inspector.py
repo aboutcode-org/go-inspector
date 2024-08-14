c43b2ce4711bb6df911b7e712a2bf8f16346fd89
Philippe Ombredanne <pombredanne@nexb.com>
2024-08-15 00:20:32 +0200

    Add new Go package handler for ScanCode
    Reference: https://github.com/aboutcode-org/go-inspector/issues/13
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_go_inspector.py
+++ tests/test_go_inspector.py
@@ -8,6 +8,7 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
+import json
 import os
 
 from pathlib import Path
@@ -20,7 +21,9 @@ from scancode.cli_test_utils import check_json_scan
 from scancode.cli_test_utils import run_scan_click
 from scancode_config import REGEN_TEST_FIXTURES
 
-from go_inspector.plugin import collect_and_parse_symbols
+from go_inspector.binary import collect_and_parse_symbols
+from go_inspector.binary import collect_go_package
+from go_inspector.binary import _collect_go_package_from_data
 
 test_env = FileDrivenTesting()
 test_env.test_data_dir = os.path.join(os.path.dirname(__file__), "data")
@@ -28,13 +31,13 @@ test_env.test_data_dir = os.path.join(os.path.dirname(__file__), "data")
 
 def test_collect_and_parse_symbols_with_plain_windows_exe():
     go_binary = test_env.get_test_loc("plain_windows.exe")
-    with pytest.raises(Exception) as e:
+    with pytest.raises(Exception):
         collect_and_parse_symbols(go_binary)
 
 
 def test_collect_and_parse_symbols_with_plain_elf():
     go_binary = test_env.get_test_loc("plain_arm_gentoo_elf")
-    with pytest.raises(Exception) as e:
+    with pytest.raises(Exception):
         collect_and_parse_symbols(go_binary)
 
 
@@ -68,3 +71,35 @@ def test_scancode_plugin_with_go_symbol_option():
     run_scan_click(args)
     expected = test_env.get_test_loc("basic/app_lin_exe-scancode.expected.json", must_exist=False)
     check_json_scan(expected, result_file, regen=REGEN_TEST_FIXTURES)
+
+
+def test_collect_go_package_with_large_go_app_linux():
+    go_binary = Path(test_env.test_data_dir).parent.parent / "src/go_inspector/bin/GoReSym_lin"
+    expected = test_env.get_test_loc("packages/GoReSym_lin-expected.json", must_exist=False)
+    results = [p.to_dict() for p in collect_go_package(location=go_binary)]
+    check_json(expected, results, regen=REGEN_TEST_FIXTURES)
+
+
+def test_collect_go_package_from_data_with_large_go_app_linux():
+    with open(test_env.get_test_loc("packages/otelcol-contrib.exe.json")) as gbd:
+        go_binary_data = json.load(gbd)
+    expected = test_env.get_test_loc("packages/otelcol-contrib.exe-expected.json", must_exist=False)
+    results = [
+        pd.to_dict()
+        for pd in
+        _collect_go_package_from_data(go_data=go_binary_data, location="some/fake/loc")
+    ]
+    check_json(expected, results, regen=REGEN_TEST_FIXTURES)
+
+
+def test_collect_go_package_from_data_with_large_go_app_linux2():
+    with open(test_env.get_test_loc("packages/tidb-server.json")) as gbd:
+        go_binary_data = json.load(gbd)
+    expected = test_env.get_test_loc("packages/tidb-server-expected.json", must_exist=False)
+    results = [
+        pd.to_dict()
+        for pd in
+        _collect_go_package_from_data(go_data=go_binary_data, location="some/fake/loc")
+    ]
+    check_json(expected, results, regen=REGEN_TEST_FIXTURES)
+
